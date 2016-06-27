08b03e0b77bf599e9c7ee105cd82b06f8d9005d3
Ingmar Jager <info@ingmarjager.nl>
2016-06-27 21:14:35 +0200

    Fix Routes

--- client/app/app.routes.ts
+++ client/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
-import { provide } from '@angular/core';
 
 import { ListAComponent } from './lista.component';
 import { ListBComponent } from './listb.component';
@@ -8,16 +6,15 @@ import { StartComponent } from './start.component';
 import { ChildAComponent } from './childa.component';
 
 export const approutes: RouterConfig = [
-  {path: '', component: StartComponent},
+  {path: '', component: StartComponent, terminal:true},
   {path: 'listA', component: ListAComponent,
      children: [
-      { path: '', component: StartComponent },
+      { path: '', component: StartComponent, terminal:true},
       { path: ':id', component: ChildAComponent }
     ]},
   {path: 'listB', component: ListBComponent},
 ];
 
 export const APP_ROUTER_PROVIDERS = [
-  provideRouter(approutes),
-  provide(APP_BASE_HREF, { useValue: '.' })
+  provideRouter(approutes)
 ];
