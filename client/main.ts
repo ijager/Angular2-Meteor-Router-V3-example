8515078bba8c4d615dcb6568f05704db0bd4b723
Ingmar Jager <info@ingmarjager.nl>
2016-07-08 00:25:47 +0200

    Update Angular and Angular-Meteor packages

--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,6 @@
 // main entry point
-//import { bootstrap } from 'angular2-meteor-auto-bootstrap';
-import { bootstrap }            from '@angular/platform-browser-dynamic';
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+//import { bootstrap }            from '@angular/platform-browser-dynamic';
 import { AppComponent }         from './app/app.component';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 
